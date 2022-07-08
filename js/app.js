40b80d47bc4640bd0367e9386c35178c9c0510c7
MoahmedALNaser <mo7ammednaser092@gmail.com>
2022-07-08 23:13:07 +0200

    create the main project structure

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import * as all from './master.js';
+console.log(all);
+console.log(all.variable);
+console.log(all.arr[0]);
+console.log(all.obj.name);
+console.log(all.func());
---
+++ js/master.js
@@ -0,0 +1,8 @@
+let variable = 'Hello World';
+let arr = [1, 2, 3, 4, 5];
+let obj = { name: 'John', age: 30 };
+let func = function sayHello() {
+    console.log('Hello World');
+}
+
+export { variable, arr, obj, func };
